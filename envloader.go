79c22d82d69441f9923d79744c21eb116a31ac54
JustinCassart <justin.casssart.4567@gmail.com>
2022-11-17 12:04:24 +0100

    adding envloader file

---
+++ envloader.go
@@ -0,0 +1,49 @@
+package goenvloader
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"github.com/mitchellh/mapstructure"
+)
+
+// Load read an environment file
+// filename (string) : the path of the environment file
+func Load(filename string, config interface{}) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	lines := bufio.NewScanner(file)
+	input := map[string]interface{}{}
+	for lines.Scan() {
+		line := lines.Text()
+		lineSplitted := strings.Split(line, "=")
+		key := lineSplitted[0]
+		value := lineSplitted[1]
+		realValue, err := strconv.Atoi(value)
+		if err == nil {
+			input[key] = realValue
+		} else {
+			input[key] = value
+		}
+	}
+	err = mapstructure.Decode(input, config) 
+	if err != nil {
+		panic(err)
+	}
+	v := reflect.ValueOf(config).Elem()
+	if v.NumField() != len(input) {
+		panic(errors.New(fmt.Sprintf("Expected %d fields but found %d\n", v.NumField(), len(input))))
+	}
+	for i := 0; i < v.NumField(); i++ {
+		if v.Field(i).IsZero() {
+			panic(errors.New("Missing value for " + v.Type().Field(i).Name + " field"))
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/JustinCassart/goenvloader
+
+go 1.19
+
+require github.com/mitchellh/mapstructure v1.5.0 // indirect
